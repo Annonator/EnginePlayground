19308462269a3c5f7badfaad62d95c2431c0eaad
Andreas Pohl <andreaspohl89@gmail.com>
2016-01-06 04:13:39 +0100

    SDL Window Test

--- main.cpp
+++ main.cpp
@@ -1,14 +1,43 @@
 #include <iostream>
 #include <SDl.h>
+#include <SDL_opengl.h>
 
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Init(SDL_INIT_VIDEO);
 
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
+  SDL_Window *window = SDL_CreateWindow("OpenGL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600,
+                                        SDL_WINDOW_OPENGL);
+
+  SDL_GLContext context = SDL_GL_CreateContext(window);
+
+  SDL_Event event;
+
+  while (true) {
+    if (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) break;
+    }
+
+    //Get window surface
+    SDL_Surface *screenSurface = SDL_GetWindowSurface(window);
+
+    //Fill the surface white
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+    //Update the surface
+    SDL_UpdateWindowSurface(window);
+
+    //Wait two seconds
     SDL_Delay(1000);
+  }
 
-    SDL_Quit();
-    return 0;
+  SDL_GL_DeleteContext(context);
+  SDL_Quit();
+  return 0;
 }
