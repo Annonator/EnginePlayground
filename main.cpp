c04c55842380e5f8242209e224f35c2790cecb48
Andreas Pohl <andreaspohl89@gmail.com>
2016-01-06 03:12:51 +0100

    SDL2 initial build test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <SDl.h>
+
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!sss" << endl;
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Delay(1000);
+
+    SDL_Quit();
     return 0;
 }
