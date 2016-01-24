f2fc60abc1c66a78346371414c637610cc9a391c
Andreas Pohl <andreaspohl89@gmail.com>
2016-01-24 18:38:22 +0100

    Added 2D Vector Class

---
+++ Engine/Math/Vector2.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Andreas Pohl on 24.01.16.
+//
+
+#include "Vector2.h"
+
+
+Engine::Vector2::Vector2() : x(1.0), y(1.0) {
+
+}
+
+Engine::Vector2::Vector2(float x, float y) : x(x), y(y) {
+
+}
---
+++ Engine/Math/Vector2.h
@@ -0,0 +1,20 @@
+//
+// Created by Andreas Pohl on 24.01.16.
+//
+
+#ifndef ENGINEPLAYGROUND_VECTOR2_H
+#define ENGINEPLAYGROUND_VECTOR2_H
+
+namespace Engine {
+class Vector2 {
+ public:
+  Vector2();
+  Vector2(float x, float y);
+
+  float x;
+  float y;
+};
+}
+
+
+#endif //ENGINEPLAYGROUND_VECTOR2_H
