db4e7b8a9e12642d907e97591e118a3984a4341c
Andreas Pohl <andreaspohl89@gmail.com>
2016-02-12 14:52:47 +0100

    Cleand Up Vector2 and moved Code To Header

--- Engine/Math/Vector2.cpp
+++ Engine/Math/Vector2.cpp
@@ -12,24 +12,3 @@ Engine::Vector2::Vector2() : x(1.0), y(1.0) {
 Engine::Vector2::Vector2(float x, float y) : x(x), y(y) {
 
 }
-
-bool Engine::Vector2::operator==(const Engine::Vector2 &rhs) const {
-  return this->x == rhs.x && this->y == rhs.y;
-}
-
-Engine::Vector2 Engine::Vector2::operator+(const Engine::Vector2 &rhs) const {
-  Vector2 newVector(this->x, this->y);
-  newVector.x = newVector.x + rhs.x;
-  newVector.y = newVector.y + rhs.y;
-
-  return newVector;
-}
-
-Engine::Vector2 Engine::Vector2::operator-(const Engine::Vector2 &rhs) const {
-  Vector2 newVector(this->x, this->y);
-  newVector.x = newVector.x - rhs.x;
-  newVector.y = newVector.y - rhs.y;
-
-  return newVector;
-}
-
--- Engine/Math/Vector2.h
+++ Engine/Math/Vector2.h
@@ -14,19 +14,27 @@ class Vector2 {
   Vector2();
   Vector2(float x, float y);
 
-  bool operator==(const Vector2 &rhs) const;
+  bool operator==(const Vector2 &rhs) const {
+    return this->x == rhs.x && this->y == rhs.y;
+  }
 
 
   friend Vector2 operator+(const Vector2 &v, const float scalar) {
     return Vector2(v.x + scalar, v.y + scalar);
   }
-  friend Vector2 operator+(const float scalar, const Vector2 &v)  {
+
+  friend Vector2 operator+(const float scalar, const Vector2 &v) {
     return Vector2(v.x + scalar, v.y + scalar);
   }
-  Vector2 operator+(const Vector2 &rhs) const;
-  Vector2 operator-(const Vector2 &rhs) const;
+
+  Vector2 operator+(const Vector2 &rhs) const {
+    return Vector2(this->x + rhs.x, this->y + rhs.y);
+  }
+
+  Vector2 operator-(const Vector2 &rhs) const {
+    return Vector2(this->x - rhs.x, this->y - rhs.y);
+  };
 };
 }
-
 
 #endif //ENGINEPLAYGROUND_VECTOR2_H
