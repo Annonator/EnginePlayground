612f834118c138918f43f4101b508ba3f2c6d66e
Andreas Pohl <andreaspohl89@gmail.com>
2016-02-13 22:47:38 +0100

    Added basic Vector 3

---
+++ Engine/Math/Vector3.cpp
@@ -0,0 +1,84 @@
+//
+// Created by Andreas Pohl on 13.02.16.
+//
+
+#include <math.h>
+#include "Vector3.h"
+
+using namespace Engine;
+
+Vector3::Vector3() : x(1.0), y(1.0), z(1.0) { }
+Vector3::Vector3(float x, float y, float z) : x(x), y(y), z(z) { }
+Vector3::~Vector3() { }
+
+float Vector3::Lenght() const {
+  return sqrtf((this->x * this->x) + (this->y * this->y) + (this->z * this->z));
+}
+
+bool Vector3::operator==(const Vector3 &rhs) const {
+  return ((this->x == rhs.x) && (this->y == rhs.y) && (this->z == rhs.z));
+}
+
+bool Vector3::operator!=(const Vector3 &rhs) const {
+  return ((this->x == rhs.x) || (this->y == rhs.y) || (this->z == rhs.z));
+}
+
+Vector3 Vector3::operator+(const Vector3 &other) const {
+  return Vector3((this->x + other.x), (this->y + other.y), (this->z + other.z));
+}
+
+Vector3 &Vector3::operator+=(Vector3 &rhs) {
+  this->x += rhs.x;
+  this->y += rhs.y;
+  this->z += rhs.z;
+
+  return *this;
+}
+
+Vector3 Vector3::operator-(const Vector3 &other) const {
+  return Vector3((this->x - other.x), (this->y - other.y), (this->z - other.z));
+}
+
+Vector3 &Vector3::operator-=(const Vector3 &rhs) {
+  this->x -= rhs.x;
+  this->y -= rhs.y;
+  this->z -= rhs.z;
+
+  return *this;
+}
+
+Vector3 Vector3::operator-() const {
+  return Vector3(-x, -y, -z);
+}
+
+Vector3 Vector3::operator*(const float scalar) const {
+  return Vector3(this->x * scalar, this->y * scalar, this->z * scalar);
+}
+
+Vector3 Engine::operator*(const float scalar, const Vector3 &vector) {
+  return Vector3(vector.x * scalar, vector.y * scalar, vector.z * scalar);
+}
+
+Vector3 &Vector3::operator*=(const float scalar) {
+  this->x *= scalar;
+  this->y *= scalar;
+  this->z *= scalar;
+
+  return *this;
+}
+
+Vector3 Vector3::operator/(const float scalar) const {
+  return Vector3(this->x / scalar, this->y / scalar, this->z / scalar);
+}
+
+Vector3 Engine::operator/(const float scalar, const Vector3 &vector) {
+  return Vector3(vector.x / scalar, vector.y / scalar, vector.z / scalar);
+}
+
+Vector3 &Vector3::operator/=(const float scalar) {
+  this->x /= scalar;
+  this->y /= scalar;
+  this->z /= scalar;
+
+  return *this;
+}
---
+++ Engine/Math/Vector3.h
@@ -0,0 +1,49 @@
+//
+// Created by Andreas Pohl on 13.02.16.
+//
+
+#ifndef ENGINEPLAYGROUND_VECTOR3_H
+#define ENGINEPLAYGROUND_VECTOR3_H
+
+namespace Engine {
+class Vector3 {
+ public:
+  float x;
+  float y;
+  float z;
+
+  Vector3();
+  Vector3(float x, float y, float z);
+  ~Vector3();
+
+  //Accessor
+  inline float &operator[](unsigned int i) { return (&x)[i]; }
+  inline float operator[](unsigned int i) const { return (&x)[i]; }
+
+  float Lenght() const;
+
+  //Compare
+  bool operator==(const Vector3 &rhs) const;
+  bool operator!=(const Vector3 &rhs) const;
+
+  //Addition / Subtraction
+  Vector3 operator+(const Vector3 &other) const;
+  Vector3 &operator+=(Vector3 &rhs);
+  Vector3 operator-(const Vector3 &other) const;
+  Vector3 &operator-=(const Vector3 &rhs);
+
+  //Invert
+  Vector3 operator-() const;
+
+  //Scalar Multiplication
+  Vector3 operator*(const float scalar) const;
+  friend Vector3 operator*(const float scalar, const Vector3 &vector);
+  Vector3 &operator*=(const float scalar);
+
+  Vector3 operator/(const float scalar) const;
+  friend Vector3 operator/(const float scalar, const Vector3 &vector);
+  Vector3 &operator/=(const float scalar);
+};
+}
+
+#endif //ENGINEPLAYGROUND_VECTOR3_H
